15d9f9b5dbcb04a116510485b2e7518a40608db7
deepa-Natarajan <53449985+deepa-Natarajan@users.noreply.github.com>
2019-12-08 15:53:14 -0600

    completed Assignment

---
+++ myscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#firstname").html("Deepa");
+    $("#lastname").html("Natarajan !");
+    $(".mcc").css('color','blue');
+    $(".mcc").css('font-style','italic');
+    $("#hobby").append("<li>Painting</li>");
+    $("#hobby").append("<li>Playing with my Kid</li>");
+
+
+});
+
+
+
